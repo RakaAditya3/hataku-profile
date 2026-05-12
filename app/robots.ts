9dadc665aac1daaf3e823198872a84f117d81932
RakaAditya3 <rakafury.ar@gmail.com>
2026-05-12 14:05:20 +0700

    Fix Robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://hatakudimsum.com'; // Update with your actual domain
+    const baseUrl = 'https://www.hataku.com'; // Update with your actual domain
 
     return {
         rules: [
