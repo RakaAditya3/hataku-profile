69fc23ef9f9ec5e3a89a4b24bb00a55c6596cf47
RakaAditya3 <rakafury.ar@gmail.com>
2026-05-12 14:02:58 +0700

    Fix SiteMap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://hatakudimsum.com'; // Update with your actual domain
+    const baseUrl = 'https://www.hataku.com'; // Update with your actual domain
 
     return [
         {
